5f25a9f361be475a20ec0cad383f35da2e20d45d
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-07 16:48:33 +0600

    cookies practice

---
+++ cookies.js
@@ -0,0 +1,12 @@
+const getCookieByName = (name) => {
+  const cookieString = document.cookie;
+  const cookieParts = cookieString.split('; ');
+  const cookie = cookieParts.find((c) => c.includes(name));
+  if (cookie) {
+    //country=US
+    const cookieValue = cookie.split('=')[1];
+    return cookieValue;
+  }
+};
+
+getCookieByName('country');
